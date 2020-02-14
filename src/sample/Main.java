8c52aaca9317be44f610480fe901607cde979b9b
Nicholas Miller <nicholascmiller123@gmail.com>
2020-02-14 12:08:10 -0500

    Displayed image

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,10 +1,26 @@
 package sample;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Main extends PApplet {
 
-    public static void main(String[] args) {
+    private PImage img;
 
+    public static void main(String[] args) {
+        PApplet.main("sample.Main");
+    }
+
+    public void settings() {
+        size(1920, 1080);
+    }
+
+    public void setup() {
+        background(0);
+        img = loadImage("stad_2.png");
+    }
+
+    public void draw() {
+        image(img, 0, 0, 2712, 1104);
     }
 }
