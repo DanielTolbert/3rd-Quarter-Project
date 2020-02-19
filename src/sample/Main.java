17e2988b4a77dbc27d1df93e5aea787e30b8a1a3
Nicholas Miller <nicholascmiller123@gmail.com>
2020-02-19 11:59:01 -0500

    Got panning and zooming working

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,11 +1,18 @@
 package sample;
 
+import javafx.scene.input.ScrollEvent;
 import processing.core.PApplet;
 import processing.core.PImage;
 
 public class Main extends PApplet {
 
     private PImage img;
+    private static final int IMAGE_WIDTH = 904;
+    private static final int IMAGE_HEIGHT = 368;
+
+    private float scale = 3;
+    private int xPos = 0;
+    private int yPos = 0;
 
     public static void main(String[] args) {
         PApplet.main("sample.Main");
@@ -21,6 +28,23 @@ public class Main extends PApplet {
     }
 
     public void draw() {
-        image(img, 0, 0, 2712, 1104);
+        image(img, xPos, yPos, IMAGE_WIDTH * scale, IMAGE_HEIGHT * scale);
+    }
+
+    public void keyPressed() {
+        if (key == CODED) {
+            switch (keyCode) {
+                case LEFT: xPos++; break;
+                case RIGHT: xPos--; break;
+                case UP: yPos--; break;
+                case DOWN: yPos++; break;
+            }
+        }
+        else {
+            switch (key) {
+                case 'w': scale += 0.01; break;
+                case 's': scale -= 0.01; break;
+            }
+        }
     }
 }
