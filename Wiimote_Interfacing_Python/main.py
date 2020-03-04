b9c4c1836297a5003dd292f98bd0529e8aa45d43
John Z <jzoscak02@comcast.net>
2020-03-04 18:36:54 -0500

    Current progress

--- Wiimote_Interfacing_Python/main.py
+++ Wiimote_Interfacing_Python/main.py
@@ -1,5 +1,13 @@
 import Wii
 
-Wii.WiiObject.__module__.
+def main():
 
+    print("Hello Timothy!")
+    sysmenu = Wii.NUS.download(0x000000010000002, 289)
+    sysmenu[0] = open("patch.bin", "rb").read()
+    sysmenu.tmd.setTitleID(0x0000000100000003)
+    sysmenu.tik.setTitleID(0x0000000100000003)
+    sysmenu.dumpFile("patched.wad")
 
+if __name__ == "__main__":
+    main()
---
+++ test2/main.py
@@ -0,0 +1,13 @@
+import Wii
+
+
+def main():
+    print("Hello Timothy!")
+    print(Wii.WiiObject.__module__)
+
+
+
+
+
+if __name__ == "__main__":
+    main()
